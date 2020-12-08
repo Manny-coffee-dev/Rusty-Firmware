63af2362330b42400a52b0b4c2478f07b2395133
Manny <manny.coffee-dev@pm.me>
2020-12-08 17:28:16 +0000

    First successful blinky

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 
 Example embedded Rust firmware for STM32G031 development board.
 
-This is heavily inspired by [Craig Bishop's blog](https://craigjb.com/2019/12/31/stm32l0-rust/).
+This is heavily inspired by:
+* [Craig Bishop's blog](https://craigjb.com/2019/12/31/stm32l0-rust/)
+* [Brian Schwind's blog](https://blog.tonari.no/rust-simple-hardware-project)
+* [Jonathan Klint's blog](https://jonathanklimt.de/electornics/programming/embedded-rust/rust-STM32F103-blink/)
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,41 @@
+// std and main are not available for bare metal software
 #![no_std]
 #![no_main]
-#[macro_use(entry)]
-extern crate cortex_m_rt as rt;
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-extern crate m;
-extern crate stm32g0;
-extern crate stm32g0xx_hal as hal;
-extern crate cortex_m;
 
-use hal::gpio::gpioa::PA15;
-use hal::gpio::{Floating, Input, Output, PushPull};
-use hal::prelude::*;
+use cortex_m::asm::delay;
+use cortex_m_rt::entry; // The runtime
+use embedded_hal::digital::v2::OutputPin; // the `set_high/low`function
+#[allow(unused_imports)]
+use panic_halt;
+use stm32g0::stm32g031;
+use stm32g0xx_hal::prelude::*; // STM32F1 specific functions // When a panic occurs, stop the microcontroller
 
-use stm32g0::stm32g031::Peripherals;
-
+// This marks the entrypoint of our application. The cortex_m_rt creates some
+// startup code before this, but we don't need to worry about this
 #[entry]
-fn entry_wrap() -> ! {
-    let p = Peripherals::take().unwrap();
+fn main() -> ! {
+    // Get handles to the hardware objects. These functions can only be called
+    // once, so that the borrowchecker can ensure you don't reconfigure
+    // something by accident.
+    let dp = stm32g031::Peripherals::take().unwrap();
+    // GPIO pins on the STM32F1 must be driven by the APB2 peripheral clock.
+    // This must be enabled first. The HAL provides some abstractions for
+    // us: First get a handle to the RCC peripheral:
+    let mut rcc = dp.RCC.constrain();
+    // Now we have access to the RCC's registers. The GPIOC can be enabled in
+    // RCC_APB2ENR (Prog. Ref. Manual 8.3.7), therefore we must pass this
+    // register to the `split` function.
+    let mut gpioa = dp.GPIOA.split(&mut rcc);
+    // This gives us an exclusive handle to the GPIOC peripheral. To get the
+    // handle to a single pin, we need to configure the pin first. Pin A15
+    // is usually connected to the Bluepills onboard LED.
+    let mut led = gpioa.pa15.into_push_pull_output();
 
-    let mut rcc = p.RCC.constrain();
-
-    // splits the GPIOA peripheral into 16 independent pins + registers
-    let mut gpioa = p.GPIOA.split(&mut rcc);
-    // all pins start as floating inputs
-    let pa15: PA15<Input<Floating>> = gpioa.pa15;
-
-    // API available in the `Input<_>` state
-
-    // configure the pin PA15 as an output
-    // this operation consumes the original `pa15` value
-    let mut pa15: PA15<Output<PushPull>> = pa15.into_push_pull_output();
-
+    // Now, enjoy the lightshow!
     loop {
-        pa15.set_low().unwrap();
+        led.set_high().ok();
+        delay(12000000);
+        led.set_low().ok();
+        delay(12000000);
     }
 }
